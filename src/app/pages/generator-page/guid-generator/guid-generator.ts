76883f5b941d563c7a8f331db1156e207bb03fd8
André Lucas de Sousa Pinto <andrelucax@gmail.com>
2026-04-10 20:31:32 -0300

    Add guid generator

--- src/app/pages/generator-page/guid-generator/guid-generator.ts
+++ src/app/pages/generator-page/guid-generator/guid-generator.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { ClipboardOutput } from '../../../shared/clipboard-output/clipboard-output';
 
 @Component({
   selector: 'dt-guid-generator',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    ClipboardOutput
+  ],
   templateUrl: './guid-generator.html',
   styleUrl: './guid-generator.scss',
 })
-export class GuidGenerator {}
+export class GuidGenerator {
+  protected output: string = "";
+
+  protected generate() {
+    this.output = crypto.randomUUID();
+  }
+}
